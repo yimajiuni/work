f028f6c0b1cce24d6e6783d142b6f2f8439ca69f
yimajiuni <106362564+yimajiuni@users.noreply.github.com>
2023-12-25 20:57:13 +0900

    Update GridPics.vue

--- mangapp/src/components/GridPics.vue
+++ mangapp/src/components/GridPics.vue
@@ -14,50 +14,32 @@
         レイアウトを削除
       </button>
       <button class="m-1 btn btn-secondary" @click="grid.compact()">整列</button>
-     <!-- {{ info }}-->
+      <!-- {{ info }}-->
     </div>
 
     <div class="grid-stack">
-      <div
-        v-for="widget in widgets"
-        :id="widget.id"
-        :key="widget.id"
-        :gs-id="widget.id"
-        :gs-x="widget.x"
-        :gs-y="widget.y"
-        :gs-h="widget.h"
-        :gs-w="widget.w"
-      >
+      <div v-for="widget in widgets" :id="widget.id" :key="widget.id" :gs-id="widget.id" :gs-x="widget.x" :gs-y="widget.y"
+        :gs-h="widget.h" :gs-w="widget.w">
         <div class="grid-stack-item-content card shadow">
           <div class="bg-light d-flex">
             <div class="flex-grow-1 draggable">
 
 
-  <div>
-    <div
-      v-for="{ id, title, image } in widget.books|| books.slice(widget.a, widget.b)"
-      :key="id"
-      class="my-3"
-    ><!--<p>{{ widget.a,widget.b }}</p>-->
-          <img
-            :src="image"
-            alt=""
-            class="card-img-top"
-          />
-          <div class="card-body">
-            <h2 class="card-title" :aria-label="title">{{ title }}</h2>
-            <div class="text-end">
-              <button
-                class="btn btn-outline-danger"
-                @click="$emit('delete-book', id)"
-              >
-                <i class="bi-trash"></i>
-                削除
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+              <div>
+                <div v-for="{ id, title, image } in widget.books || books.slice(widget.a, widget.b)" :key="id"
+                  class="my-3"><!--<p>{{ widget.a,widget.b }}</p>-->
+                  <img :src="image" alt="" class="card-img-top" />
+                  <div class="card-body">
+                    <h2 class="card-title" :aria-label="title">{{ title }}</h2>
+                    <div class="text-end">
+                      <button class="btn btn-outline-danger" @click="$emit('delete-book', id)">
+                        <i class="bi-trash"></i>
+                        削除
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
             </div>
             <div>
@@ -68,32 +50,29 @@
           </div>
           <div class="col d-flex align-items-center justify-content-center">
             <!-- Display widget content or additional functionality here -->
-            <test-modal-three
-            class="ml-auto"
-            @add-book="onAddBook"
-          >
-          </test-modal-three>
+            <test-modal-three class="ml-auto" @add-book="onAddBook">
+            </test-modal-three>
             <span class="text">{{ widget.id }}</span>
           </div>
         </div>
       </div>
-  </div>
+    </div>
 
   </div>
-  </template>
+</template>
 
-  <script>
-  import { onMounted, nextTick, ref } from "vue";
-  import "gridstack/dist/gridstack.min.css";
-  import "gridstack/dist/gridstack-extra.css";
-  import "gridstack/dist/gridstack-extra.min.css";
-  import { GridStack } from 'gridstack';
-  import TestModalThree from '@/components/TestModalThree.vue';
+<script>
+import { onMounted, nextTick, ref } from "vue";
+import "gridstack/dist/gridstack.min.css";
+import "gridstack/dist/gridstack-extra.css";
+import "gridstack/dist/gridstack-extra.min.css";
+import { GridStack } from 'gridstack';
+import TestModalThree from '@/components/TestModalThree.vue';
 
-  export default {
-    components: {
-     TestModalThree,
-    },
+export default {
+  components: {
+    TestModalThree,
+  },
   props: {
     books: {
       type: Array,
@@ -105,64 +84,63 @@
     };
   },
   setup(props) {
-//setup内のmethodからpropsのbooksにアクセスするためにpropsをsetupに渡し、booksにはpropsを代入  
     const grid = ref(null);
     const widgets = ref([
       { id: "initWidget-1", w: 2, h: 2, a: 0, b: 1, },
-      { id: "initWidget-2", w: 2, h: 2, a: 1, b: 2  },
-      { id: "initWidget-3", w: 2, h: 2, a: 2, b: 3  },
-      { id: "initWidget-4", w: 2, h: 2, a: 3, b: 4  },
+      { id: "initWidget-2", w: 2, h: 2, a: 1, b: 2 },
+      { id: "initWidget-3", w: 2, h: 2, a: 2, b: 3 },
+      { id: "initWidget-4", w: 2, h: 2, a: 3, b: 4 },
     ]);
     const info = ref("");
-    
-  const nextBook = (widget) => {
-    widget.a++;
-    widget.b++;
-    console.log("Updated a, b:", widget.a, widget.b);//ここ◯
-  };
 
-  const loadLayout = () => {
-      const savedData = JSON.parse(localStorage.getItem("gridstack-layout") || "[]");
+    const nextBook = (widget) => {
+      widget.a++;
+      widget.b++;
+      console.log("Updated a, b:", widget.a, widget.b);
+    };
+    const loadLayout = () => {
+      const savedData = JSON.parse(localStorage.getItem("gridstack-layout") || "{}");
+      console.log("おっと", savedData);
+
       if (savedData.layouts) {
-    grid.value.load(savedData.layouts); // Load the layout data into GridStack
-  }
+        grid.value.load(savedData.layouts);
+      }
 
-  if (savedData.widgets) {
-    widgets.value = savedData.widgets; // Update your custom widget data
-  }
+      if (savedData.widgets) {
+        widgets.value = savedData.widgets;
+
+      }
     };
 
     const addNewWidget = () => {
       const uniqueId = Date.now().toString(16);
-      console.log(`おっとっと`,props.books);
+      console.log(`おっとっと`, props.books);
 
-      
+
       const widget = {
         id: `widget-${uniqueId}`,
         w: Math.floor(Math.random() * 2) ? 2 : 4,
         h: Math.floor(Math.random() * 2) ? 2 : 4,
         a: widgets.value.length,
-        b: widgets.value.length+1,
-        
+        b: widgets.value.length + 1,
+
       };
       widgets.value.push(widget);
-      
-      //addwidget後のdisplay
+
       nextTick(() => {
         makeWidget(widget);
-        /*nextBook(widget);*/
-        console.log("After nextBook:", widget.a, widget.b);//ここ◯
+        console.log("After nextBook:", widget.a, widget.b);
 
       });
     };
 
-    const clearWidgets= () => {
+    const clearWidgets = () => {
       widgets.value = [];
       grid.value.removeAll();
       console.warn("おっとっと１")
     };
 
-    const deleteWidget= (id) =>  {
+    const deleteWidget = (id) => {
       const index = widgets.value.findIndex((widget) => widget.id === id);
       if (index === -1) {
         return;
@@ -172,35 +150,30 @@
       grid.value.removeWidget(elSelector);
       widgets.value.splice(index, 1);
     };
-
     const saveLayout = () => {
-//books arrayの値に対して2つの値を切り出す処理を繰り返す
-    const widgetsWithBooks = widgets.value.map((widget) => {
-    const subsetStart = widget.a;
-    const subsetEnd = widget.b;
-    const subset = props.books.slice(subsetStart, subsetEnd);
-    console.log("おっとっと3");
-    return {
-      ...widget,
-      books: subset,
-      a: subsetStart, // Include the 'a' property in the widget
-      b: subsetEnd,   // Include the 'b' property in the widget
-    };
-  });
-//変形後レイアウト上書き保存できない＞このラインで出来る、がデータがダブル
-  //新規絵をポスト後保存できない(サーバーに保存はされている)
-//セーブの仕方の問題
-  const layoutData = {
-    layouts: grid.value.save(), // Save the layout data generated by GridStack
-    widgets: widgetsWithBooks,   // Save your custom widget data
-  };
+      const layouts = grid.value.save();
+      const widgetsWithBooks = widgets.value.map((widget) => {
+        const subsetStart = widget.a;
+        const subsetEnd = widget.b;
+        const subset = props.books.slice(subsetStart, subsetEnd);
+        return {
+          ...layouts,
+          ...widget,
+          books: subset,
+          a: subsetStart,
+          b: subsetEnd,
+        };
+      }); 
 
-  localStorage.setItem("gridstack-layout", JSON.stringify(layoutData));
-};
+      const layoutData = {
+        widgets: widgetsWithBooks,
+      };
+
+      localStorage.setItem("gridstack-layout", JSON.stringify(layoutData));
+    };
 
     const deleteLayout = () => {
       localStorage.removeItem("gridstack-layout");
-      console.log("おっとっと4");
     };
 
     const makeWidget = (widget) => {
@@ -214,14 +187,14 @@
       });
     };
 
-    
+
 
     const compactGrid = () => {
       grid.value.compact();
     };
 
-  const onAddBook = async (book) => {
-    this.books.push(book);
+    const onAddBook = async (book) => {
+      this.books.push(book);
       this.$emit('add-book', book);
       console.log("おっとっと追加");
     };
@@ -235,7 +208,7 @@
         maxRow: 20,
         acceptWidgets: true,
       });
-//ロード後のレイアウトと絵
+
       loadLayout();
       nextTick(() => {
         makeWidgets();
@@ -245,8 +218,6 @@
         info.value = `you just dragged node #${node?.id} to {x: ${node?.x}, y: ${node?.y}}`;
       });
     });
-//any variable, function, or reactive object that you want to use in the template or
-// expose to other parts of your component needs to be included in the return object of the setup function. 
     return {
       grid,
       widgets,
@@ -264,11 +235,11 @@
 };
 
 
-  </script>
+</script>
   
-  <style scoped>
-  .draggable {
-    cursor: move;
-  }
-  </style>
+<style scoped>
+.draggable {
+  cursor: move;
+}
+</style>
   
